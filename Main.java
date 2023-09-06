c10faba212bb163cbfdf82e449fea3aafcb40337
akshay2857 <jawaleakshay5229@gmail.com>
2023-09-06 16:03:10 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-class Main{
-     public static void main(String[] args) {
-    System.out.println("this is java pgms with jenkins");
+class Main {
+    public static void main(String[] args) {
+        System.out.println("this is java pgms with jenkins");
+        System.out.println("hello");
     }
 }
